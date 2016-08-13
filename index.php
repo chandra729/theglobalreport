94c7a10b4b1b004f7254491fbaa66f79124134f6
chandra shekhar <chandrashekhar729@gmail.com>
2016-08-13 15:03:31 +0200

    changed the index file to main_index

--- index.php
+++ index.php
@@ -1,122 +1,47 @@
-<div id="wrapper">
-<?php
-include 'header.php';
-?> 
- 
- <div id="content">
-    <!-- begin featured ---------------------------------------------------------------------------------------------------------------->
-    <div id="featured">
-      <div class="post">
-	  
-    <!----------------------------------------------------------------------------------------------------------------------------->   
-     <h2> <a href="/featured/20160809-I-am-normal.php">I'm a real person, but people see me just as a martyr: Irom Sharmila </a></h2>
-        <p class="details"> Aug 10, 2016, 02.05 PM IST | <a href="#">TimesOfIndia</a> </p>
-        <div class="thumb"><a href="#"><img src="images/I-am-normal.png" alt="IronSharmila" /></a></div>
-        <p>Manipuri activist Irom Sharmila wants people to know she's a real person.</p>
-        <p>"...But I'm viewed as a strange person," Sharmila told Times Now, a day after she broke her 16-year fast. If you could call two spoons of honey and half a glass of Horlicks an actual meal, that is.
-        The publicity and the 'celebrity' surrounding her is still fazing her, it appears.</p>
-        <p class="readmore">[ <a href="/featured/20160809-I-am-normal.php">read more</a> ]</p>
-		<div class="break"></div>
-      </div>  
-    <!-------------------------------------------------------------------------------------------------->  
-            
-       <div class="post">
-   
+<html>
+  <head>
+    <title>Welcome to theglobalreports.com</title>
+  </head>
+  <body>
 
-       <h2><a href="#">Donald Trump's Muslim ban: From simple clarity to plain confusion</a></h2>
-        <p class="details"> JUN 28 2016 | <a href="#">TimesOfIndia</a> </p>
-        <div class="thumb"><a href="#"><img src="images/_featured2.JPG" alt="" /></a></div>
-        <p> <a href="http://localhost/attacker/victim/login.php?username=%27%20OR%20%271%27=%271&password=%27%20OR%20%271%27=%271">From the moment </a> he first declared it, the plan has been a signature of his campaign for president: "Donald J Trump is calling for a total and complete shutdown of Muslims entering the United States until our country's representatives can figure out what is going on." 
-	<p class="readmore">[ <a href="/featured/test.php">read more</a> ]</p> 
-      <div class="break"></div>
-      </div>
-    </div>
-    <!-- end featured ---------------------------------------------------------------------------------------------------------->
-   
+<!---
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '869701586495704',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
 
-     <!-- begin recent posts -->
-      <div class="recent">
-      <!-- begin post ----------------------------------------------------------------------------------------------------------->
-   <!--   <div class="o post"> <a href="#"><img src="images/_thumb.jpg" alt="" /></a>
-        <h2></h2>
-        <p>  -->
-               <?php
-                    include 'subpost1.php';
-               ?> 
-
-    <!--       </p>
-        <p class="readmore">[ <a href="#">read more</a> ]</p>
-        <div class="break"></div>
-      </div> ---> 
-      <!-- end post ----------------------------------------------------------------------------------------------------------------->
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 
 
 
-      <!-- begin post --------------------------------------------------------------------------------------------------------------->
-   <!--   <div class="e post"> <a href="#"><img src="images/_thumb.jpg" alt="" /></a>
-        <h2><a href="#"> </a></h2>
-        <p> --> 
-            <?php
-                    include 'subpost2.php';
-               ?> 
+<div
+  class="fb-like"
+  data-share="true"
+  data-width="450"
+  data-show-faces="true">
+</div>  --> 
 
-     <!--      </p>
-        
-<p class="readmore">[ <a href="#">read more</a> ]</p>
-        <div class="break"></div>
-      </div> ---> 
-  <!-- end post --------------------------------------------------------------------------------------------------------------------->
-     
-    
 
- <!-- begin post ------------------------------------------------------------------------------------------------------------->
-  <!--    <div class="o post"> <a href="#"><img src="images/_thumb.jpg" alt="" /></a>
-        <h2><a href="#"> </a></h2>
-        <p>  ---> 
-               <?php
-                    include 'subpost3.php';
-               ?> 
-            
 
-       <!-- </p> 
-        <p class="readmore">[ <a href="#">read more</a> ]</p>
-        <div class="break"></div>
-      </div> --->
-      <!-- end post ----------------------------------------------------------------------------------------------------------------------->
-     
-
- <!-- begin post --------------------------------------------------------------------------------------------------------------------->
-    <!--  <div class="e post"> <a href="#"><img src="images/_thumb.jpg" alt="" /></a>
-        <h2><a href="#">Special Twitter Icons for Sale</a></h2>
-        <p>
-                  -->
-             <?php
-                    include 'subpost4.php';
-               ?> 
-
-      <!---  </p>
-        <p class="readmore">[ <a href="#">read more</a> ]</p>
-        <div class="break"></div>
-      </div>  ---> 
-       <!-- end post ------------------------------------------------------------------------------------------------------------------------>
-	  
-    </div>
-    <!-- end recent posts -------------------------------------->
-  </div>
-  <!-- END content ---------------------------------------------------------------------------->
-  
   <?php
-          include 'sidebar.php';
-    ?>
+     echo "<h1>"."Welcome to theglobalreports.com"."</h1>";
+     echo "<h2>"."Website would be online soon.Thanks."."</h1>";
+   ?>  
 
-	 </div>  	
-<!-- END wrapper -------------------------------------------------------------------------------------------------->
- 
-<?php	 
-     include 'footer.php';
-  ?>  
 
- 
-  
-  
-  
+
+
+</body>
+</html>
+
