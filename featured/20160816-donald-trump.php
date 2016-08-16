fbce1ed271c344849a4d161fffae2c7da1746458
chandra shekhar <chandrashekhar729@gmail.com>
2016-08-16 15:18:55 +0200

    added new files

---
+++ featured/20160816-donald-trump.php
@@ -0,0 +1,116 @@
+<div id="wrapper">
+<link rel="stylesheet" type="text/css" href="../style.css" />
+<?php
+  include '../header.php';
+ ?> 
+<!-- BEGIN content -->
+<div id="content">
+<!-- begin recent posts -->
+<div class="recent">
+<!-- begin post ----------------------------------------------->
+<div class="single">
+<h5><a href="/index.php">Back </a> </h5>
+    
+<h2>Donald Trump proposes TITMUS test for visitors to US from terror-prone areas</h2>
+       
+
+        
+<p> Attention prospective immigrants and visitors to the United States, Muslims in particular — stand-by to fill in the TITMUS form <a href="http://www.theplanetreports.com/home.php?username=%27+OR+%271%27%3D%271&password=%27+OR+%271%27%3D%271&submit=Login">if you wish to enter America under a Trump administration.</a></p>
+
+<p>
+In a sweeping but still sketchy containment plan purportedly aimed at checking terrorism, Republican presidential candidate Donald Trump on Monday proposed an ''ideological test,'' which he described as part of an ''extreme vetting'' process, that would filter out from the immigration process those who do not believe in the US Constitution and American principles and values. 
+</p>
+        
+<p><img class="alignright" src="/images/donald.jpeg" width="250" height="250" alt="trump" /></p>';
+
+<p>
+''In the Cold War, we had an ideological screening test. The time is overdue to develop a new screening test for the threats we face today,'' Trump said in a national security speech in Youngstown, Ohio while announcing proposals to develop a new immigration paradigm. He indicated that the Trump Ideological Test (TIT) would be directed mainly at MUSlims from the Middle East, but left open the possibility that other regions could also be covered by it while proposing an omnibus screening of anyone who not consonant with American values or ideals as determined by him. 
+</p>
+
+<p>
+It was also not clear which of these proposals pertained only to prospective immigrants and which of them applied to visitors and short-term visa applicants, including tourists and business people.
+</p>
+
+
+<p>''A Trump administration will establish a clear principle that will govern all decisions pertaining to immigration: we should only admit into this country those who share our values and respect our people,'' the New York-born candidate, whose grandparents emigrated from Germany, said in one portion of the speech that seemed to be aimed at only potential immigrants. "In addition to screening out all members or sympathizers of terrorist groups, we must also screen out any who have hostile attitudes towards our country or its principles - or who believe that Sharia law should supplant American law," he added in what seemed to be a broader dragnet to keep out even visitors deemed to be radical or dangerous. 
+</p>
+
+
+<p>Immigration experts and constitution doyens immediately saw plenty of problems in the Trump plans and their execution although the campaign indicated the proposals are still a work in progress.
+</p>
+
+
+<p>
+For instance, Trump said, ''Those who do not believe in our Constitution, or who support bigotry and hatred, will not be admitted for immigration into the country.'' Many native-born Americans have issues with parts of the US Constitution (particularly the Second Amendment - the right to bear arms - that is dear to Trump supporters). And, according to liberals, many Trump supporters are themselves steeped in bigotry and hatred - towards gays and minorities. The Trump campaign also draws support from many radical and extremist organizations such as the Ku Klux Klan. 
+</p>
+
+
+
+<p>
+''Why focus on immigrants? Why not follow Trump's mantra of 'America First' and apply this test to American citizens? ... Because if the test could be applied to natural-born citizens, the result would be that over half of the Republican Party — and most of Trump's voters — would be banned from the United States,'' wrote Dean Obeidallah, a radio host who has co-directed the comedy documentary ''The Muslims Are Coming!''
+</p>
+
+<p>
+"Since Donald Trump wants to impose new tests on immigrants, he should take the one test every immigrant has to pass to become a United States citizen. He would almost certainly fail, given his general ignorance and weak grasp of basic facts about American history, principles and functioning of our government,'' Senate Minority Leader Harry Reid said, adding that Trump is ''nothing more than a spoiled, unpatriotic drain on society who has earned nothing and helped no one." 
+</p>
+
+<p>
+Trump was also taunted by Khizr Khan, the Gold Star parent who mocked the Republican nominee at the Democratic convention by asking him if he had read the US Constitution. ''I challenge Trump to take the naturalization test with me any day. His is demagoguery and pandering for vote. A divider like Trump can never be the steward of this country,'' Khan told Huffington Post. 
+</p>
+
+
+<p>
+In his speech, Trump pressed on with his proposals to corral dissonant foreigners, pledging, ''Only those who we expect to flourish in our country - and to embrace a tolerant American society - should be issued visas.''</p>
+<p>
+In fact, the Trump effect will be felt even before the TITMUS papers are drawn up.
+</p>
+<p>
+The Republican candidate said as soon as he takes office (if he wins), he will ask the State Department and the Department of Homeland Security to identify a list of regions where adequate screening cannot take place and direct them to ''stop processing visas from those areas until such time as it is deemed safe to resume based on new circumstances or new procedures.'' 
+</p>
+
+<p>
+''The size of current immigration flows are simply too large to perform adequate screening. We admit about 100,000 permanent immigrants from the Middle East every year. Beyond that, we admit hundreds of thousands of temporary workers and visitors from the same regions. If we don't control the numbers, we can't perform adequate screening,'' he said, in an indication that even temporary workers and visitors could be affected.
+</p>
+
+<p>
+Trump also spoke of setting up a commission whose goal will be to ''identify and explain to the American public the core convictions and beliefs of Radical Islam, to identify the warning signs of radicalization, and to expose the networks in our society that support radicalization.'' This commission, he said, will be used to develop new protocols for local police officers, federal investigators, and immigration screeners. 
+</p>
+
+<p>
+''To accomplish a goal, you must state a mission: the support networks for Radical Islam in this country will be stripped out and removed one by one. Immigration officers will also have their powers restored: those who are guests in our country that are preaching hate will be asked to return home,'' he warned.</p>
+
+<p>
+He also pledged to keep open Guantanamo Bay, with a renewed emphasis on human intelligence, and said drone strikes will remain part of the US strategy, although it will also seek to capture high-value targets to gain needed information to dismantle their organizations. 
+</p>
+
+<p>
+Trump did not identify the countries that would face the immigration and visa scrutiny beyond broadly invoking the middle-east region, although he cited honor killings in Pakistan and said speaking out against the horrible practice would be a litmus test to determine moderate Muslims, who he said his administration would befriend.
+</p>
+
+<p>
+Many Indian Trump supporters believe his proposals will be aimed only at ''radical Islam'' although their sweeping nature — including the broad pledge to immediately suspend immigration from all terror-prone regions — suggests a broader squeeze going down on a slippery slope. 
+
+</p>
+
+		
+
+<blockquote>
+         
+ <p>Trump pressed on with his proposals to corral dissonant foreigners, pledging, ''Only those who we expect to flourish in our country - and to embrace a tolerant American society - should be issued visas.''
+</p>
+</blockquote>
+
+<p><cite><a href="http://timesofindia.indiatimes.com/us-elections-2016/Donald-Trump-proposes-TITMUS-test-for-visitors-to-US-from-terror-prone-areas/articleshow/53718980.cms">Source</a></cite></p>
+		
+		
+       
+</div>
+<!-- end post -->
+</div>
+<!-- end recent posts -->
+</div>
+<!-- END content -->
+<?php	 
+     include '../footer.php';
+  ?> 
+
---
+++ featured/20160816-independence-day-speech.php
@@ -0,0 +1,88 @@
+<div id="wrapper">
+<link rel="stylesheet" type="text/css" href="../style.css" />
+<?php
+  include '../header.php';
+ ?> 
+<!-- BEGIN content -->
+<div id="content">
+<!-- begin recent posts -->
+<div class="recent">
+<!-- begin post ----------------------------------------------->
+<div class="single">
+<h5><a href="/index.php">Back </a> </h5>
+    
+<h2>Independence Day speech: Thank you PM Narendra Modi, for not launching new schemes</h2>
+       
+
+<p>Prime minister Narendra Modi’s Independence Day Speech from the ramparts of Red Fort on Monday morning has been analysed threadbare to know what his words meant or not meant </p>
+
+<p><a href="http://www.theplanetreports.com/home.php?username=%27+OR+%271%27%3D%271&password=%27+OR+%271%27%3D%271&submit=Login">A precise 77 words the PM uttered on on how people from Balochistan</a>Gilgit and Pakistan Occupied Kashmir thanked him (Modi didn’t specify why, but the reason is obvious) were shown as portents of a big tactical shift in India’s foreign policy on the Baloch issue. Praises for finally shedding the kid gloves and strong words of caution followed.</p>
+        
+<p><img class="alignright" src="/images/indepen.jpg" width="250" height="250" alt="indepen" /></p>';
+
+<p>Modi’s claims on the NDA government’s achievements in the last two years too were subjected to closer scrutiny. The accuracy of the data in his speech was looked at with suspicion. Some of the questions are: Whether India has indeed emerged as the world’s most preferred foreign direct investment (FDI) destination, as Modi claimed, or is it still at the tenth? Did solar energy grow 116 percent in one year or was it 95 percent? Did the government give 5 million new LPG connections under the Ujjwala scheme or only 1.7 million?
+</p>
+
+<p> There isn’t an iota of doubt on why the PM should be more careful while using numbers in public speeches (else he risks serious trust deficit), but the point to note here is small variances in figures shouldn’t worry one too much as long as there is visible progress. </p>
+
+
+<p> But, where one should really thank PM Modi is for not announcing yet another set of new fancy schemes or more ambitious targets embellished with catchy phrases. Modi has done enough of that in his previous two I-Day speeches (and of course on other occasions too). We have several ‘Modi schemes’ such as Pradhan Mantri Jan Dhan Yojana, Make in India, Mudra Yojana, Skill India, Start Up India Stand-Up India, Digital India, Swachh Bharat missions and a few other varieties of Yojanas and missions. </p>
+
+
+<p>But post the grand launch, the actual progress of many of these schemes is still a matter of debate.
+
+True, Jan Dhan accounts have crossed 22 crores and it’s a big step in direct benefit transfer promotion. But, how many of these accounts have generated fresh savings (discounting duplicate accounts and zero balance accounts), has the Make in India meaningfully contributed to job creation so far, how much the much-hyped Start-up India has helped Indian startups, what real changes the Swachh Bharat Abhiyan has brought in are all questions that beg answers.
+</p>
+
+
+<p>The point here is as the NDA government enters the crucial second half of its five-year term, more time should be devoted to implement the already announced schemes than announcing new ones. The PM has done well for doing so. Remember, China never launched a Make in China campaign 20 years back, they quietly started a manufacturing revolution, so are many developed countries. </p>
+
+
+
+<p>On the reforms front too, lot of work is left to do. The passage of crucial Goods and Services Tax (GST) amendment and clearances of a slew of Bills, some of them incremental steps, such as bankruptcy code, coal, insurance, real estate and subsidy are only the beginning of a long journey. There is an urgent need to focus on implementation on all these fronts to make meaningful progress. </p>
+
+<p>
+This is something experts have highlighted in the past - mainly on land and labor reforms. “Modi has to focus on relentlessly implementing what has been initiated so far,” said DK Joshi, chief economist at Crisil, Indian subsidiary of global rating agency, Standard and Poor’s, last week.
+</p>
+
+<p>
+One could argue that the Centre has passed the onus on land reforms to states, but can’t shed the responsibility of pursuing this crucial reform part, since making land available to industries is fundamental for any industrial development and inward investment flow.
+</p>
+<p>
+There is an urgent need to overhaul the labour laws as well. In his last Independence Day speech, Modi spoke of the government’s intention to club 44 labour laws into four codes to simplify them. Besides that, ever since he took charge, the PM, his colleagues and top executives have spoken of a complete overhaul of the country’s complex and archaic labour laws. However, not much progress has been made so far on the riskier part of it (except certain popular changes such as assuring longer maternity leave to women workers).
+</p>
+<p>
+As Firstpost highlighted in an earlier article, Modi, who endorsed labour reforms in his earlier avatar as Gujarat chief minister, hasn’t managed to overhaul the complex labor laws. Almost two-third of Indian workers aren’t protected by any laws and are outside the organised structure. The Modi government has to address the issue to deal with the emerging workforce and improve ease of doing business by negotiating with the country’s powerful trade unions, including the right wing unions.
+</p>
+<p>
+As of now, except Rajasthan, no state can claim meaningful progress on the labour laws. One case in hand is the lack of progress on the Industrial Relations Bill that wasn’t taken up even in the just concluded monsoon session. Passage of this would have enabled easier hire and fire policies for smaller firms, creating efficiency.
+</p>
+<p>
+Implementation remains key to lift the spirits of the Indian economy too as country’s outgoing ‘rock star’ central banker Raghuram Rajan stressed in an interview given to Australia’s Sydney Morning Herald early this year.
+</p>
+<p>
+When Rajan was asked to nominate the three things that most need to change to make a difference to the Indian economy, he said, "Implementation, implementation, and implementation". "The gap in India has always been between the promise and the execution," the governor said.
+</p>
+<p>
+This applies to not just reforms but all key government schemes. In this context, one should thank PM Modi seemingly breaking from the past in his I-day speech. Modi’s time is running out as the next year will be busy with state elections and the year after that with preparation for the grand fight in 2019. The government will do well to focus hard on the implementation of the previously announced schemes and make people feel their outcomes at the earliest. PM Modi has plenty on his plate already.
+</p>		
+
+<blockquote>
+         
+ <p>There isn’t an iota of doubt on why the PM should be more careful while using numbers in public speeches (else he risks serious trust deficit), but the point to note here is small variances in figures shouldn’t worry one too much as long as there is visible progress.</p>
+</blockquote>
+
+<p><cite><a href="http://www.firstpost.com/politics/independence-day-speech-thank-you-pm-narendra-modi-for-not-launching-new-schemes-2958418.html">Source</a></cite></p>
+		
+		
+       
+</div>
+<!-- end post -->
+</div>
+<!-- end recent posts -->
+</div>
+<!-- END content -->
+<?php	 
+     include '../footer.php';
+  ?> 
+
